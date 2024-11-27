2de6feada23d236c57b6d286740d90c7600c9b8b
vale-og <139730895+vale-og@users.noreply.github.com>
2024-11-26 22:47:50 -0300

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -9,19 +9,31 @@ def main(initial_star_number = 100, seed = 1):
     STEPS
 
     1) Generate data
-    random_probs: random values from 0 to 1
-    random_masses: random values from 0.08 to 100 Msun
-    random_ages: random values from 0 to 13.9 Gyr (MW age)
+    random_probs: random uniform probabilities with values from 0 to 1
+    random_masses: random uniform masses from 0.08 to 100 Msun
 
-    2) Build a dataframe with all the stars, including the IMF probability (Kroupa (2001)), and the MS lifetime 
+    2) Calculate the IMF probability for each mass, and eliminate those with random probability larger than IMF probability
 
-    3) Filter the dataframe to select stars in and out of the IMF limits, and in and out of the main sequence
+    3) Generate random ages from a uniform distribution for all stars below the IMF boundary, and calculate Main Sequence (MS) lifetimes using the mass values
 
-    4) Assing remnant labels to stars that are out of the main sequence
-    remnants: classification (1 == 'wd', 2 == 'ns', 3 == 'bh') according to the initial mass
+    4) Classify the star as MS or evolved, as follows:
+    age < t_MS: not evolved, still in MS
+    age > t_MS: evolved
 
-    5) Obtain final mass for each star according to the remnant type
-    final_mass: calculated according to Kalirai for stars that have evolved out of the main sequence
+    5) Assing the respective remnant labels to the evolved stars, based on their initial masses
+
+    7) Calculate final masses for the stars according to the remnant type and their mass
+
+    8) Create two dataframes:
+
+    ms_stars: All stars in the MS, that are below the IMF limit; includes only mass and ages
+
+    evolved_stars: Stars that have evolved out of the MS; includes mass, age, remnant type and final mass
+
+    Both of these dataframes are saved in separate .csv files once the program finishes
+
+    9) OPTIONAL: plot the distribution of masses and ages for MS and evolved stars
+
 
     RETURNS
     A file containing the masses, ages, remnant type and final mass for stars out of the MS
@@ -106,6 +118,7 @@ def main(initial_star_number = 100, seed = 1):
     df = pd.DataFrame({'mass': masses_in_imf[is_evolved],
                        'age': random_ages[is_evolved],
                        'remnant': remnants,
+                       'is_evolved': is_evolved[is_evolved],
                        'final_mass': final_masses})
     df.to_csv('evolved_stars.csv', index = False)
     
@@ -124,7 +137,7 @@ def main(initial_star_number = 100, seed = 1):
     return  input("Would you like to plot results? (y/n): ")
 
 
-def plots(plot_ms, plot_evolved, fig_path):
+def plots(plot_ms, plot_evolved):
     import matplotlib.pyplot as plt
     import numpy as np
     import pandas as pd
@@ -139,25 +152,27 @@ def plots(plot_ms, plot_evolved, fig_path):
         
         fig, axs = plt.subplots(nrows = 1, ncols = 2,  figsize = (15,10))
 
+        weights = np.ones(len(ms_stars))/float(len(ms_stars))
         
-        axs[0].hist(ms_stars['mass'], bins = 100, color = 'red', alpha = 0.4, density = False, range = (0, max_mass))
+        axs[0].hist(ms_stars['mass'], bins = 100, color = 'red', alpha = 0.4, weights = weights, range = (0, max_mass))
         axs[0].set_xlabel('M [M$_\\odot$]')
-        axs[0].set_ylabel('Counts')
+        axs[0].set_ylabel('Density')
 
 
-        axs[1].hist(ms_stars['age'], bins = 100, color = 'red', alpha = 0.4, density = False, range = (0, max_mass))
-        axs[1].set_xlabel('M [M$_\\odot$]')
-        axs[1].set_ylabel('Counts')
+        axs[1].hist(ms_stars['age'], bins = 100, color = 'red', alpha = 0.4, weights = weights, range = (0, max_mass))
+        axs[1].set_xlabel('Age [Gyr]')
+        axs[1].set_ylabel('Density')
 
         fig.suptitle('Mass and Age distribution for MS stars')
 
-        plt.savefig(fig_path + 'ms_mass_age_distribution.pdf', dpi = 300)
+        plt.tight_layout()
+        plt.savefig('ms_mass_age_distribution.pdf', dpi = 300)
 
         plt.show()
 
 
-
-        input('Click ENTER to go to next plot')
+        if plot_evolved == 'y':
+            input('Click ENTER to go to next plot')
 
     
     if plot_evolved == 'y':
@@ -165,61 +180,78 @@ def plots(plot_ms, plot_evolved, fig_path):
         fig, axs = plt.subplots(nrows = 2, ncols = 3, figsize = (15,15))
 
         max_mass = evolved_stars['final_mass'][evolved_stars['remnant'] == 1].max()
-        
-        axs[0][0].hist(evolved_stars['final_mass'][evolved_stars['remnant'] == 1] , bins = 100, color = 'gray', alpha = 0.4, density = False, label = 'WD', range = (0, max_mass))
+        weights = np.ones(len(evolved_stars[evolved_stars['remnant'] == 1]))/float(len(evolved_stars[evolved_stars['remnant'] == 1]))
+
+        axs[0][0].hist(evolved_stars['final_mass'][evolved_stars['remnant'] == 1] , bins = 100, color = 'gray', alpha = 0.4, weights = weights, label = 'WD', range = (0, max_mass))
+
+
+
 
         max_mass = evolved_stars['final_mass'][evolved_stars['remnant'] == 2].max()
-        
-        axs[0][1].hist(evolved_stars['final_mass'][evolved_stars['remnant'] == 2] , bins = 100, color = 'blue', alpha = 0.4, density = False, label = 'NS', range = (0, max_mass))
+        weights = np.ones(len(evolved_stars[evolved_stars['remnant'] == 2]))/float(len(evolved_stars[evolved_stars['remnant'] == 2]))
+
+        axs[0][1].hist(evolved_stars['final_mass'][evolved_stars['remnant'] == 2] , bins = 100, color = 'blue', alpha = 0.4, weights = weights, label = 'NS', range = (0, max_mass))
+
+
+
 
         max_mass = evolved_stars['final_mass'][evolved_stars['remnant'] == 3].max()
-        
-        axs[0][2].hist(evolved_stars['final_mass'][evolved_stars['remnant'] == 3] , bins = 100, color = 'black', alpha = 0.4, density = False, label = 'BH', range = (0, max_mass))
+        weights = np.ones(len(evolved_stars[evolved_stars['remnant'] == 3]))/float(len(evolved_stars[evolved_stars['remnant'] == 3]))
+
+        axs[0][2].hist(evolved_stars['final_mass'][evolved_stars['remnant'] == 3] , bins = 100, color = 'black', alpha = 0.4, weights = weights, label = 'BH', range = (0, max_mass))
 
 
 
 
         
         max_age = evolved_stars['age'][evolved_stars['remnant'] == 1].max()
-        
-        axs[1][0].hist(evolved_stars['age'][evolved_stars['remnant'] == 1] , bins = 100, color = 'gray', alpha = 0.4, density = False, label = 'WD', range = (0, max_age))
+        weights = np.ones(len(evolved_stars[evolved_stars['remnant'] == 1]))/float(len(evolved_stars[evolved_stars['remnant'] == 1]))
+
+        axs[1][0].hist(evolved_stars['age'][evolved_stars['remnant'] == 1] , bins = 100, color = 'gray', alpha = 0.4, weights = weights, label = 'WD', range = (0, max_age))
+
+
 
         max_age = evolved_stars['age'][evolved_stars['remnant'] == 2].max()
-        
-        axs[1][1].hist(evolved_stars['age'][evolved_stars['remnant'] == 2] , bins = 100, color = 'blue', alpha = 0.4, density = False, label = 'NS', range = (0, max_age))
+        weights = np.ones(len(evolved_stars[evolved_stars['remnant'] == 2]))/float(len(evolved_stars[evolved_stars['remnant'] == 2]))
+
+        axs[1][1].hist(evolved_stars['age'][evolved_stars['remnant'] == 2] , bins = 100, color = 'blue', alpha = 0.4, weights = weights, label = 'NS', range = (0, max_age))
+
+
 
         max_age = evolved_stars['age'][evolved_stars['remnant'] == 3].max()
-        
-        axs[1][2].hist(evolved_stars['age'][evolved_stars['remnant'] == 3] , bins = 100, color = 'black', alpha = 0.4, density = False, label = 'BH', range = (0, max_age))
+        weights = np.ones(len(evolved_stars[evolved_stars['remnant'] == 3]))/float(len(evolved_stars[evolved_stars['remnant'] == 3]))
+
+        axs[1][2].hist(evolved_stars['age'][evolved_stars['remnant'] == 3] , bins = 100, color = 'black', alpha = 0.4, weights = weights, label = 'BH', range = (0, max_age))
 
 
 
 
         
         axs[0][0].set_xlabel('M [M$_\\odot$]')
-        axs[0][0].set_ylabel('Counts')
+        axs[0][0].set_ylabel('Density')
         axs[0][1].set_xlabel('M [M$_\\odot$]')
-        axs[0][1].set_ylabel('Counts')
+        axs[0][1].set_ylabel('Density')
         axs[0][2].set_xlabel('M [M$_\\odot$]')
-        axs[0][2].set_ylabel('Counts')
+        axs[0][2].set_ylabel('Density')
         axs[1][0].set_xlabel('Age [Gyr]')
-        axs[1][0].set_ylabel('Counts')
+        axs[1][0].set_ylabel('Density')
         axs[1][1].set_xlabel('Age [Gyr]')
-        axs[1][1].set_ylabel('Counts')
+        axs[1][1].set_ylabel('Density')
         axs[1][2].set_xlabel('Age [Gyr]')
-        axs[1][2].set_ylabel('Counts')
+        axs[1][2].set_ylabel('Density')
 
         axs[1][0].legend()
         axs[1][1].legend()
         axs[1][2].legend()
 
-        fig.suptitle('Mass and Age distribution for evolved stars')
+        fig.suptitle('Final Mass and Age distribution for evolved stars')
 
+        plt.tight_layout()
 
-        plt.savefig(fig_path + 'evolved_mass_age_distribution.pdf', dpi = 300)
+        plt.savefig('evolved_mass_age_distribution.pdf', dpi = 300)
         plt.show()
 
-    return print(f'Check your plots at {fig_path}')
+    return print('Done! :)')
 
 
 
@@ -237,9 +269,8 @@ if __name__ == "__main__":
         
         plot_ms = input('Would you like to plot the mass and age distribution of the main sequence stars? (y/n): ') 
         plot_evolved = input('Would you like to plot the mass and age distribution of the evolved stars? (y/n): ') 
-        fig_path = input('Directory to save files: ') 
 
-        plots(plot_ms, plot_evolved, fig_path)
+        plots(plot_ms, plot_evolved)
         
     else:
         print('Done! :)')
